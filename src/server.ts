acf5dd63c0fc1e4ca16ba3d35543e18e23385e31
Karabo Mashao <karabom@leantechnovations.com>
2026-08-28 13:46:07 +0200

    Architecture is a modular monolith with a layered structure.

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,15 @@ import express, {type Request, type Response} from "express";
 const app = express();
 const PORT = 3000;
 
+
+app.get("/", (req, res) =>{
+    res.send("This is the home page or whatever it is that I want to do")
+})
+
+app.get("/admin", (req, res) => {
+    res.send("You have reached the Admin portal")
+})
+
 app.listen(PORT, () => {
     console.log(`Server is runnin on http://localhost:${PORT}`);
 });
